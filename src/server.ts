42ecbf82f2bb21b0439f4b36faa52afa29a8a2f8
Ezequiel-Molinari/FS73 <molinariezequiel2@gmail.com>
2026-07-07 17:39:02 -0300

    feat: cliente y job de exchange rates con cache en DB

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,18 @@ import app from './app';
 import { ENV } from './config/env';
 import { logger } from './config/logger';
 import { testDatabaseConnection } from './db/pool';
+import { fetchAndStoreExchangeRates } from './jobs/fetch-exchange-rates.job';
 
 const PORT = ENV.PORT || 5000;
 
 testDatabaseConnection()
+  .then(() => fetchAndStoreExchangeRates())
   .then(() => {
     app.listen(PORT, () => {
       logger.info(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    logger.error('No se pudo conectar a la DB:', err);
+    logger.error('Error al iniciar el server:', err);
     process.exit(1);
   });
